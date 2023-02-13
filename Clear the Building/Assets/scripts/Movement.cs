5d5663e145cc86a66c8ae2a73a208c873c0e9b73
IRUNBOSS <nuri.onacak@hotmail.com>
2023-02-13 22:24:06 +0300

    geliştirmeler

--- Clear the Building/Assets/scripts/Movement.cs
+++ Clear the Building/Assets/scripts/Movement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
+    [SerializeField] private float turnSpeed = 15f;
+    [SerializeField] private Transform rayStartPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +24,43 @@ public class Movement : MonoBehaviour
 
     private void TakeInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&OnGroundCheck())
         {
-            rb.velocity = new Vector3(rb.velocity.x, (jumpPower * 100) * Time.deltaTime, 0f);
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp((jumpPower * 100) * Time.deltaTime, 0f, 15f), 0f);
         }
-        else
-        {
 
-        }
+
         if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector3((-speed * 100) * Time.deltaTime, rb.velocity.y, 0f);
+            rb.velocity = new Vector3(Mathf.Clamp((-speed * 100) * Time.deltaTime,-15f,0f), rb.velocity.y, 0f);
+            //transform.rotation = Quaternion.Euler(0f,180f,0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 179.99f, 0f), turnSpeed* Time.deltaTime);
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector3((speed * 100) * Time.deltaTime, rb.velocity.y, 0f);
+            rb.velocity = new Vector3(Mathf.Clamp((speed * 100) * Time.deltaTime,0f,15f), rb.velocity.y, 0f);
+            //transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0.01f, 0f), turnSpeed*Time.deltaTime);
 
+
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
+        }
+    }
+    private bool OnGroundCheck()
+    {
+        bool hit = Physics.Raycast(rayStartPoint.position, Vector3.down, 0.5f);
+        Debug.DrawRay(rayStartPoint.position, Vector3.down * 0.5f,Color.red);
+        if (hit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
